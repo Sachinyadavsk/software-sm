3293eede7a6f3bcb5198a7164aa430115856ebb4
Sachinyadavsk <sachinsk4832499@gmail.com>
2026-03-30 12:49:43 +0530

    user access add in backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './db/config.js';
+import userRoutes from "../routes/userRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -12,11 +13,14 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+// Routes
+app.use("/api/users", userRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server running on port ${PORT}`);
+  connectDB();
+  console.log(`Server running on port ${PORT}`);
 });
