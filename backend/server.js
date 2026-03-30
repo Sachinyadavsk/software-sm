daf0aae37f7098c75fb6dc3be8defc455a79a9c2
Sachinyadavsk <sachinsk4832499@gmail.com>
2026-03-30 13:04:40 +0530

    error

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './db/config.js';
-import userRoutes from "../routes/userRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 
 const app = express();
